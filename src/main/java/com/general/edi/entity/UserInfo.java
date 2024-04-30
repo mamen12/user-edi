111e76ddefe9964d70b6b13b8d77d1f7fff16e61
Syahrul Ilham Muttaqien <syahrulilham62@gmail.com>
2024-04-30 20:34:58 +0700

    initial commit

---
+++ src/main/java/com/general/edi/entity/UserInfo.java
@@ -0,0 +1,62 @@
+package com.general.edi.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name="mt_user")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfo implements Serializable { 
+  
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name; 
+    
+    @Column(name = "nama_lengkap")
+    private String namaLengkap; 
+    
+    private String password;
+    
+    @Column(name = "created_at")
+    private Date createdAt;
+    
+    @Column(name = "created_by")
+    private String createdby;
+    
+    @Column(name = "updated_at")
+    private Date updatedAt;
+    
+    @Column(name = "updated_by")
+    private String updatedBy;
+    
+    @Column(name = "deleted_at")
+    private Date deletedAt;
+    
+    @Column(name = "deleted_by")
+    private String deletedBy;
+
+    @Column(name = "last_login")
+    private Date lastLogin;
+}
+
---
+++ src/test/java/com/general/edi/EdiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.general.edi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
